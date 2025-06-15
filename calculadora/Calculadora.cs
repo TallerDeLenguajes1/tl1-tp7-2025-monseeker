f8f8b251ab65b4713646075212b123fe513df45b
monseeker <guesam460@gmail.com>
2025-06-15 00:56:55 -0300

    Ejercicio 1 terminado

--- calculadora/Calculadora.cs
+++ calculadora/Calculadora.cs
@@ -1,22 +1,44 @@
-namespace EspacioCalculadora {
-
-    public class Calculadora {
+namespace EspacioCalculadora
+{
+    public class Calculadora
+    {
         private double dato;
-        public Calculadora(double datoPorPArametro = 0){
-            dato = datoPorPArametro;
-        }
-        public void Sumar(double termino){
+
+        public void Sumar(double termino)
+        {
             dato += termino;
         }
 
-        public void Dividir(double termino){
-            if(termino != 0) dato = dato/termino;
+        public void Restar(double termino)
+        {
+            dato -= termino;
         }
 
-        public double GetResultado(){
+        public void Multiplicar(double termino)
+        {
+            dato *= termino;
+        }
+
+        public void Dividir(double termino)
+        {
+            if (termino != 0)
+            {
+                dato /= termino;
+            }
+            else
+            {
+                Console.WriteLine("No es posible dividir por cero.\n");
+            }
+        }
+
+        public void Limpiar()
+        {
+            dato = 0;
+        }
+
+        public double Resultado()
+        {
             return dato;
         }
-
     }
-
 }
--- calculadora/Program.cs
+++ calculadora/Program.cs
@@ -1,14 +1,85 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using EspacioCalculadora;
 
-Console.WriteLine("Hello, World!");
+Calculadora calc = new Calculadora();
+bool otraOperacion = true;
+Console.WriteLine("Calculadora");
+do
+{
+    Console.WriteLine("1. Sumar");
+    Console.WriteLine("2. Restar");
+    Console.WriteLine("3. Multiplicar");
+    Console.WriteLine("4. Dividir\n");
+    Console.WriteLine("Seleccione la operacion: ");
+    string cadena = Console.ReadLine();
+    if (double.TryParse(cadena, out double elegirOperacion))
+    {
+        Console.WriteLine("Ingrese un numero: ");
+        cadena = Console.ReadLine();
+        if (double.TryParse(cadena, out double num))
+        {
+            switch (elegirOperacion)
+            {
+                case 1:
+                    calc.Sumar(num);
+                    Console.WriteLine($"Resultado: {calc.Resultado()}\n");
+                    break;
+                case 2:
+                    calc.Restar(num);
+                    Console.WriteLine($"Resultado: {calc.Resultado()}\n");
+                    break;
+                case 3:
+                    calc.Multiplicar(num);
+                    Console.WriteLine($"Resultado: {calc.Resultado()}\n");
+                    break;
+                case 4:
+                    calc.Dividir(num);
+                    Console.WriteLine($"Resultado: {calc.Resultado()}\n");
+                    break;
+                default:
+                    Console.WriteLine("Ingrese una opcion valida.\n");
+                    break;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Ingrese un numero.\n");
+        }
 
-Calculadora miCalculadora = new Calculadora();
+        bool valido = false;
+        do
+        {
+            Console.WriteLine("Desea realizar otra operacion?");
+            Console.WriteLine("1.Si \t2.No\n");
+            cadena = Console.ReadLine();
+            if (int.TryParse(cadena, out int desicion))
+            {
+                if (desicion == 1)
+                {
+                    valido = true;
+                    otraOperacion = true;
+                }
+                else if (desicion == 2)
+                {
+                    valido = true;
+                    otraOperacion = false;
+                }
+                else
+                {
+                    Console.WriteLine("Ingrese un numero valido.\n");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ingrese un numero.\n");
+            }
+        } while (!valido);
 
-miCalculadora.Sumar(23);
-miCalculadora.Sumar(10);
-miCalculadora.Sumar(50);
-miCalculadora.Sumar(2);
-miCalculadora.Dividir(2);
+    }
+    else
+    {
+        Console.WriteLine("Ingrese un numero.\n");
+    }
 
-Console.WriteLine($"El resultado es: {miCalculadora.GetResultado()}");
+} while (otraOperacion);
+calc.Limpiar();
